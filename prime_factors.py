63e069d8f564df213e64ddda33b65b6ba7b24ce8
junhwik <wjdrhdsbrlfl@gmail.com>
2025-07-31 12:32:45 +0900

    [feature] p.27

--- prime_factors.py
+++ prime_factors.py
@@ -1,2 +1,4 @@
 class PrimeFactors:
-    ...
+
+    def of(self, param):
+        return []
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -2,5 +2,9 @@ from prime_factors import PrimeFactors
 
 
 def test_prime_factors():
-    prime_factor = PrimeFactors
+    prime_factor = PrimeFactors()
     assert 1==1
+
+def test_prime_factors_of_1():
+    prime_factor = PrimeFactors()
+    assert prime_factor.of(1) == []
