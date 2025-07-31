17a7997e5e094aaa5c2a7ee86b98704a13856d87
junhwik <wjdrhdsbrlfl@gmail.com>
2025-07-31 12:40:54 +0900

    [refactor] p.40

--- prime_factors.py
+++ prime_factors.py
@@ -3,9 +3,10 @@ class PrimeFactors:
     def of(self, num):
         factors=[]
         if num>1:
-            if num == 4:
-                factors.append(2)
-                factors.append(2)
+            if num==4:
+                while num %2==0:
+                    factors.append(2)
+                    num//=2
             else:
                 factors.append(num)
         return factors
